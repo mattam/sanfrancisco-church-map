49fec0e3a1fc1a1434d2a1780f62d4cecb94d0b5
Matthew Chan <mattam@gmail.com>
2020-10-17 14:16:59 -0700

    Adding in a number of churches mapped

--- pages/components/Map/index.js
+++ pages/components/Map/index.js
@@ -14,34 +14,37 @@ export default function MapComponent(props) {
     zoom: 12,
   };
   return props.churches ? (
-    <Map
-      center={[state.lat, state.lng]}
-      zoom={state.zoom}
-      style={{ width: "500px", height: "500px" }}
-    >
-      <TileLayer
-        attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {props.churches.map(({ attributes: church }) => {
-        if (church.NLat) {
-          const point = [church.NLat, church.NLong];
-          return (
-            <Marker position={point} key={church.FID}>
-              <Popup>
-                <h2>{church["Name"]}</h2>
-                <span>
-                  ADDRESS: {church["Address"]}, {church["City"]} -{" "}
-                  {church["ZIP"]}
-                </span>
-                <br />
-                <br />
-              </Popup>
-            </Marker>
-          );
-        }
-      })}
-    </Map>
+    <div>
+      <Map
+        center={[state.lat, state.lng]}
+        zoom={state.zoom}
+        style={{ width: "500px", height: "500px" }}
+      >
+        <TileLayer
+          attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {props.churches.map(({ attributes: church }) => {
+          if (church.NLat) {
+            const point = [church.NLat, church.NLong];
+            return (
+              <Marker position={point} key={church.FID}>
+                <Popup>
+                  <h2>{church["Name"]}</h2>
+                  <span>
+                    ADDRESS: {church["Address"]}, {church["City"]} -{" "}
+                    {church["ZIP"]}
+                  </span>
+                  <br />
+                  <br />
+                </Popup>
+              </Marker>
+            );
+          }
+        })}
+      </Map>
+      <h3>Number of churches mapped: {props.churches.length}</h3>
+    </div>
   ) : (
     "Data is loading..."
   );
