29a25ba5c88a4d2fa099f5a3397974235e612407
Matthew Chan <mattam@gmail.com>
2020-10-17 10:50:03 -0700

    Pulling in the YWAM data and cleaning it up

---
+++ data/extractMapData.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+var esridata = require("./ywam-churchdata.json");
+
+// console.log("esridata.operationalLayers", esridata.operationalLayers);
+
+let churches = [];
+
+esridata.operationalLayers.forEach((layer) => {
+  if (
+    layer.layerType === "ArcGISFeatureLayer" &&
+    layer.title === "Sacred Places"
+  ) {
+    churches = layer.featureCollection.layers[0].featureSet.features;
+  }
+});
+
+// console.log("churches: ", churches);
+let count = 0;
+let missingLat = 0;
+churches.map((church) => {
+  const data = church.attributes;
+  console.log("data: ", data);
+  count++;
+  if (data.NLat === null || data.NLat === undefined) {
+    missingLat++;
+  }
+});
+
+console.log(`found ${count} churches`);
+console.log(`${missingLat} churches are missing Latitudes`);
+
+let writeData = JSON.stringify(churches);
+fs.writeFileSync("ywam-cleanedchurchdata.json", writeData);
